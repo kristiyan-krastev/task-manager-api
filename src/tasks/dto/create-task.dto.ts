a140173c99550eead770b184f644e1da64f2fb18
Kristiyan Krastev <k.krastev.1998@gmail.com>
2022-09-13 23:35:10 +0300

    Change the create user and task dtos to have validation on their fields.

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTaskDto {}
+import { IsOptional, MaxLength } from "class-validator";
+
+export class CreateTaskDto {
+
+  @MaxLength(100, {
+    message: "Title too long. Maximum length is $constraint1, but actual is $value"
+  })
+  title: string;
+
+  @IsOptional()
+  @MaxLength(5000, {
+    message: "Description too long. Maximum length is $constraint1, but actual is $value"
+  })
+  description: string;
+}
