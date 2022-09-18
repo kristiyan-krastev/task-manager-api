e055bcf2a932e51cc53b60286786e98a532473b0
Kristiyan Krastev <k.krastev.1998@gmail.com>
2022-09-18 14:19:17 +0300

    Implement the User entity; Implement the create user endpoint; Add User entity to UsersModule imports; Implement create method in users service by using the UserRepository;

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'users', synchronize: false})
+export class User {
+  @PrimaryGeneratedColumn({name: 'id'})
+  id: number;
+
+  @Column({name: 'firstName'})
+  firstName: string;
+
+  @Column({name: 'lastName'})
+  lastName: string;
+}
